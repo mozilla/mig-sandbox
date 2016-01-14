a74e0e6735575dd6f447e9835f900292dfa1cb49
Alexandru Tudorica <tudalex@gmail.com>
2016-01-14 21:20:59 +0200

    Revert "Check if seccomp is supported and do not exit"
    This reverts commit 5810e1c35a31e1d747b20caa33baeb49c8bfe60d.

--- sandbox_linux.go
+++ sandbox_linux.go
@@ -70,11 +70,7 @@ func Jail(sandboxProfile SandboxProfile) {
 	filter.SetNoNewPrivsBit(true)
 	err = filter.Load()
 	if err != nil {
-		if err != errBadFilter {
-			log.Printf("Error loading seccomp filter, seccomp (filter mode) not supported by kernel: %s. Sandbox features disabled.", err)
-		} else {
-			log.Fatal("Error loading filter: %s", err)
-		}
+		log.Fatal("Error loading filter: %s", err)
 	} else {
 		log.Printf("Loaded filter\n")
 	}
